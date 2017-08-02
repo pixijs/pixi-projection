9c32069dbc1e61297debc207f3f169fe9842a5a9
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-08-02 18:20:13 +0300

    derp projection bounds

--- src/curve/BaseSurface.ts
+++ src/curve/BaseSurface.ts
@@ -15,6 +15,12 @@ namespace pixi_projection {
 
 		}
 
+		/**
+		 * made for bilinear, other things will need adjustments, like test if (0) is inside
+		 * @param {ArrayLike<number>} v
+		 * @param out
+		 * @param {PIXI.Matrix} after
+		 */
 		boundsQuad(v: ArrayLike<number>, out: any, after?: PIXI.Matrix) {
 			tempPoint.set(v[0], v[1]);
 			this.apply(tempPoint, tempPoint);
@@ -29,30 +35,30 @@ namespace pixi_projection {
 			if (after) {
 				after.apply(tempPoint, tempPoint);
 			}
-			if (minX > v[2]) minX = v[2];
-			if (maxX < v[2]) maxX = v[2];
-			if (minY > v[3]) minY = v[3];
-			if (maxY < v[3]) maxY = v[3];
+			if (minX > tempPoint.x) minX = tempPoint.x;
+			if (maxX < tempPoint.x) maxX = tempPoint.x;
+			if (minY > tempPoint.y) minY = tempPoint.y;
+			if (maxY < tempPoint.y) maxY = tempPoint.y;
 
 			tempPoint.set(v[4], v[5]);
 			this.apply(tempPoint, tempPoint);
 			if (after) {
 				after.apply(tempPoint, tempPoint);
 			}
-			if (minX > v[4]) minX = v[4];
-			if (maxX < v[4]) maxX = v[4];
-			if (minY > v[5]) minY = v[5];
-			if (maxY < v[5]) maxY = v[5];
+			if (minX > tempPoint.x) minX = tempPoint.x;
+			if (maxX < tempPoint.x) maxX = tempPoint.x;
+			if (minY > tempPoint.y) minY = tempPoint.y;
+			if (maxY < tempPoint.y) maxY = tempPoint.y;
 
 			tempPoint.set(v[6], v[7]);
 			this.apply(tempPoint, tempPoint);
 			if (after) {
 				after.apply(tempPoint, tempPoint);
 			}
-			if (minX > v[6]) minX = v[6];
-			if (maxX < v[6]) maxX = v[6];
-			if (minY > v[7]) minY = v[7];
-			if (maxY < v[7]) maxY = v[7];
+			if (minX > tempPoint.x) minX = tempPoint.x;
+			if (maxX < tempPoint.x) maxX = tempPoint.x;
+			if (minY > tempPoint.y) minY = tempPoint.y;
+			if (maxY < tempPoint.y) maxY = tempPoint.y;
 
 			out[0] = minX;
 			out[1] = minY;
