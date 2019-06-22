f69d136f061712dc6d983e97d28325610282db78
djmisterjon <24865815+djmisterjon@users.noreply.github.com>
2019-06-22 09:33:29 -0400

    ObservableEuler enumerability
    cb,scope not enumerable for json

--- src/proj3d/ObservableEuler.ts
+++ src/proj3d/ObservableEuler.ts
@@ -11,6 +11,8 @@ namespace pixi_projection {
 
 	export class ObservableEuler implements PIXI.PointLike, Euler {
 		constructor(public cb: any, public scope: any, x?: number, y?: number, z?: number) {
+			Object.defineProperty(this, cb   , { enumerable: false });
+			Object.defineProperty(this, scope, { enumerable: false });
 			/**
 			 * @member {number}
 			 * @default 0
